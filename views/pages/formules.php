2b647c25b6aac6b5034be3936259b58088b2548e
Alexia <alexia.j67@gmail.com>
2026-03-03 10:19:25 +0100

    Création du fichier theme.css et rangement de tous les fichiers .css dans un dossier styles.

--- views/pages/formules.php
+++ views/pages/formules.php
@@ -2,34 +2,38 @@
 include_once '../../views/components/_header.php';
 ?>
 
-<main>
-    <section id="sessions" class="">
-
-        <h2 class="">Nos Formules</h2>
-
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 bg-midnight-blue text-neutral-200 p-4">        
-
+<main class="bg-midnight-blue font-roboto text-neutral-200 pt-15">
+    <section id="formules">
+        <div class="">
+            <h2 class="text-center text-xl font-bold uppercase justify-center align-center">Nos Formules</h2>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 mt-10">        
+    
             <article class="corner-frame p-8" id="card">
                 <div>
-
-                <h3 class="text-center text-xl font-bold mb-8">Adultes Indoor</h3>
-                <img src="" alt="" aria-hidden="true">
+                    <h3 class="text-center text-xl font-bold mb-8">Adultes Indoor</h3>
+                    <img src="" alt="" aria-hidden="true">
                 </div>
 
-                <p class="">15 ans et +</p>
-                <ul>
-                    <li>1 session : 17 €</li>
-                    <li>2 sessions : 32 €</li>
-                    <li>3 sessions : 44 €</li>
-                </ul>
-                <p>Une session dure 20 minutes</p>
-                <a href="#" class="btn-primary">Réserver</a>
+                <div>
+                    <p class="">15 ans et +</p>
+                    <ul>
+                        <li>1 session : 17 €</li>
+                        <li>2 sessions : 32 €</li>
+                        <li>3 sessions : 44 €</li>
+                    </ul>
+                    <p>Une session dure 20 minutes</p>
+                    <a href="#" class="btn-primary">Réserver</a>
+                </div>
             </article>
 
             <article class="corner-frame p-8" id="card">
-                <img class="" alt="" src="" aria-hidden="true">
                 <div>
                     <h3 class="text-center text-xl font-bold mb-8">Adultes Outdoor</h3>
+                    <img class="" alt="" src="" aria-hidden="true">
+                </div>
+              
+                <div>                    
                     <p>15 ans et +</p>
                     <ul>
                         <li>1 session : 20 € par adulte</li>
@@ -42,9 +46,12 @@ include_once '../../views/components/_header.php';
             </article>
 
             <article class="corner-frame p-8" id="card">
-                <img class="" src="" alt="" aria-hidden="true">
                 <div>
                     <h3 class="text-center text-xl font-bold mb-8">Enfants Indoor</h3>
+                    <img class="" src="" alt="" aria-hidden="true">
+                </div>
+               
+                <div>                    
                     <p>de 7 à 14 ans</p>
                     <ul>
                         <li>1 session : 15 €</li>
@@ -54,7 +61,8 @@ include_once '../../views/components/_header.php';
                     <p>Une session dure 20 minutes</p>
                     <a href="#" class="btn-primary">Réserver</a>
                 </div>
-            </article>
+            </article>  
         </div>
+    </section>      
 </main>
 
