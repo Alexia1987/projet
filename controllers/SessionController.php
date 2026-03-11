766875b3b985531f3688bcc4fde86b7dc71f626d
Alexia <alexia.j67@gmail.com>
2026-03-11 16:42:09 +0100

    passé en paramètre des deux méthodes generateSlots() et displaySlotsInCalendar() et appel de SessionModel en dehors de celles-ci.

--- controllers/SessionController.php
+++ controllers/SessionController.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
+require_once __DIR__ . '/../models/SessionModel.php';
 
 class SessionController {
 
-function renderCalendar(string $page, array $slots = []): void {   
+function renderCalendar(string $page, array $slots = []): void {
     // extract() convertit les clés de tableau en noms de variable et les valeurs de tableau en valeur de variable.
     // Entrée : array("a" => "jean", "b" => "alex", "c" => "bob")
     // Sortie : $a = "jean" , $b = "alex" , $c = "bob"
@@ -10,14 +11,11 @@ function renderCalendar(string $page, array $slots = []): void {
     include __DIR__ . "/../views/pages/$page.php";
 }
 
-function generateSlots(string $startDate, string $endDate): int {
-    require_once __DIR__ . '/../models/SessionModel.php';
+function generateSlots(PDO $pdo, string $startDate, string $endDate): int {
     return insertSlots($pdo, 2, $startDate, $endDate, 10, 25.0);
 }
 
-function displaySlotsInCalendar(): void {
-    require_once __DIR__ . '/../models/SessionModel.php';
-
+function displaySlotsInCalendar(PDO $pdo): void {
     // 'calendar' → inclura views/pages/calendar.php
     // $slots sera disponible directement dans la vue
     $slots = getSlots($pdo);
