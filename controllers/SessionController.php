1c70e763a4fa21e40ac2cfb4b2708f567e49eea4
Alexia <alexia.j67@gmail.com>
2026-03-11 14:56:02 +0100

    Suppression des variables non utilisées et du foreach dans la fonction generateSlots() du SessionCtrl.

--- controllers/SessionController.php
+++ controllers/SessionController.php
@@ -2,7 +2,7 @@
 
 class SessionController {
 
-private function renderCalendar(string $page, array $slots = []): void {   
+function renderCalendar(string $page, array $slots = []): void {   
     // extract() convertit les clés de tableau en noms de variable et les valeurs de tableau en valeur de variable.
     // Entrée : array("a" => "jean", "b" => "alex", "c" => "bob")
     // Sortie : $a = "jean" , $b = "alex" , $c = "bob"
@@ -10,38 +10,12 @@ private function renderCalendar(string $page, array $slots = []): void {
     include __DIR__ . "/../views/pages/$page.php";
 }
 
-public function generateSlots(): void {
-
+function generateSlots(string $startDate, string $endDate): int {
     require_once __DIR__ . '/../models/SessionModel.php';
-    
-    // Appel de la fonction définie dans SessionModel.php  
-    insertSlots($pdo, 2, $startDate, $endDate, 10, 25.0);
-
-    foreach ($slots as $slot) {
-    // 'H' = Heure, au format 24h, avec les zéros initiaux (de 00 à 23)
-    $hour = (int) date('H', $slot);
-
-    // fonction: date('w') format utilisé: US (0-6, dim=0)
-    // fonction: date('N') format utilisé: ISO (1-7, lun=1)
-    $StdDayOfWeek = (int) date('N', $slot);
-
-    $isOpen = match ($StdDayOfWeek) {
-        1, 2 => false, // lundi, mardi : fermé
-        3    => $hour >=10 && $hour <=21,  // mercredi
-        4    => $hour >=14 && $hour <=21,  // jeudi
-        5    => $hour >=14 && $hour <=23,  // vendredi
-        6    => $hour >=9 && $hour <=23,   // samedi
-        7    => $hour >=9 && $hour <=21,   // dimanche
-        default => false
-    };
-    if (!$isOpen) {
-        continue; // passe au jour suivant
-    }
-}
+    return insertSlots($pdo, 2, $startDate, $endDate, 10, 25.0);
 }
 
-public function displaySlotsInCalendar(): void {
-
+function displaySlotsInCalendar(): void {
     require_once __DIR__ . '/../models/SessionModel.php';
 
     // 'calendar' → inclura views/pages/calendar.php
