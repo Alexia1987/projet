1555d0ab42c06667890316b87673e4255488b9fc
Alexia <alexia.j67@gmail.com>
2026-04-12 19:46:08 +0200

    Ajout de la logique JS pour la gestion de la réservation de créneau horaire.

--- views/pages/calendar.php
+++ views/pages/calendar.php
@@ -11,11 +11,17 @@
     <div id="event-modal" class="relative z-10 bg-dark-blue-steel rounded-xl p-8 w-full max-w-md shadow-xl">
         <button id="event-modal-close" class="absolute top-4 right-4 text-grey-blue-secondary hover:text-light cursor-pointer">✕</button>
         <h2 id="event-modal-title" class="text-fusion-orange text-xl font-bold mb-2"></h2>
-        <p id="event-modal-datetime" class="text-light text-sm mb-1"></p>
-        <div>
-            <button id="event-modal-enter" class="bg-fusion-orange hover:bg-amber-500 rounded-md text-midnight-blue text-s mt-4 p-2 cursor-pointer">Réserver</button>
+        <p id="event-modal-datetime" class="text-light text-m mb-1"></p>
+        <div>           
             <button id="event-modal-cancel" class="bg-grey-blue-secondary hover:bg-gray-300 rounded-md text-midnight-blue text-s mt-4 p-2 cursor-pointer">Annuler</button>
         </div>
+        <form id="booking-form" method="POST" action="/reserver">
+            <label class="text-light text-m">Veuillez indiquer le nombre de participants :
+                <input type="hidden" id="session_id_input" name="session_id_input" value="">
+                <input type="text" id="participants_input" name="participants_input" class="bg-light text-midnight-blue mt-4">
+                <button type="submit" id="event-modal-submit" class="bg-fusion-orange hover:bg-amber-500 rounded-md text-midnight-blue text-s mt-4 p-2 cursor-pointer">Réserver</button>
+            </label>
+        </form>
     </div>
 </div>
 
@@ -35,22 +41,36 @@
                 right: 'dayGridMonth,timeGridWeek,timeGridDay',
             },
             eventClick: function(info) {       
-
+                
                 const modal = document.getElementById('event-modal-container');
                 const start = info.event.start;
                 const end = info.event.end;
 
+                // Formatage date/heure de chaque créneau horaire
                 const dateFmt = (d) => d.toLocaleDateString('fr-FR', { 
                     day: '2-digit', month: '2-digit'});
                 const timeFmt = (d) => d.toLocaleTimeString('fr-FR', { 
                     hour: '2-digit', minute: '2-digit'});
 
+                // Modal qui apparaît au clic sur un créneau horaire
                 const modalTitle = document.getElementById('event-modal-title');
                 modalTitle.textContent = "Passer à la réservation ?";
-
                 const modalDatetime = document.getElementById('event-modal-datetime');
                 modalDatetime.textContent = "Créneau sélectionné : le " + dateFmt(start) + " de " + timeFmt(start) + " h à " + timeFmt(end) + " h";
-                modal.classList.remove('hidden'); 
+
+                // Pour récupérer l'id du créneau horaire cliqué (session_id)
+                // On récupère la valeur de l'élément 'input' caché dans le formulaire        
+                const sessionId = document.getElementById('session_id_input').value = info.event.id;
+                modal.classList.remove('hidden');
+
+                // Ensuite on doit envoyer cette donnée en même temps que 
+                // l'entrée de l'utilisateur (nombre de participants pour le créneau réservé)
+                // L'envoi du formulaire doit se faire au clic sur le bouton "Réserver"
+                const bookingForm = document.getElementById('booking-form');
+                const submitBtn = document.getElementById('event-modal-submit');
+                submitBtn.addEventListener('click', function(event) {
+                    bookingForm.submit();
+                })
             },
 
             eventMouseEnter: function(info) {
@@ -72,20 +92,20 @@
         });
         calendar.render();
 
-        // Fermeture de la fenêtre modal
+        // Fermeture de la fenêtre modal...
         const modal = document.getElementById('event-modal-container');
 
-        // Au clic sur le bouton 'X'
+        // ...Au clic sur le bouton 'X'
         document.getElementById('event-modal-close').addEventListener('click', function() {
             modal.classList.add('hidden');
         });
 
-         // Au clic sur le bouton Annuler
+         // ...Au clic sur le bouton Annuler
         document.getElementById('event-modal-cancel').addEventListener('click', function() {
             modal.classList.add('hidden');
         });
 
-        // Au clic sur l'overlay, en dehors de la modal
+        // ...Au clic sur l'overlay, en dehors de la modal
         document.getElementById('event-modal-overlay').addEventListener('click', function() {
             modal.classList.add('hidden');
         });
