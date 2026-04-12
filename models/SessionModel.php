04d0dec00071a94adffe36a0c0d663ca5c91998f
Alexia <alexia.j67@gmail.com>
2026-04-12 19:39:43 +0200

    Suppression de la fonction addBooking.

--- models/SessionModel.php
+++ models/SessionModel.php
@@ -143,24 +143,3 @@ function getRemainingPlaces(PDO $pdo): array {
 
     return $remainingPlaces;
 }
-
-
-function addBooking(PDO $pdo, $userId, $sessionId, $participants) {
-    try {
-        $sql = "INSERT INTO `booking`(`bkg_user_id`, `bkg_session_id`, `bkg_nb_of_participants`)
-                        VALUES(:user_id, :session_id, :participants)";
-
-        $query = $pdo->prepare($sql);
-
-        $result = $query->execute([
-            ':user_id'      => $userId,
-            ':session_id'   => $sessionId,
-            ':participants' => $participants
-        ]);
-            return $result ? "Votre réservation a été effectuée avec succès." : "Une erreur technique est survenue.";
-
-    } catch (PDOException $e) {
-
-        return "Une erreur technique est survenue.";
-}
-}
