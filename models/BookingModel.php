acc853a54182a6c0a38b4901affe85150911eb34
Alexia <alexia.j67@gmail.com>
2026-04-13 18:07:48 +0200

    Suppression des appels de fichiers inutiles et modification de la gestion des messages d'erreur.

--- models/BookingModel.php
+++ models/BookingModel.php
@@ -1,11 +1,9 @@
 <?php
-$pdo = require_once __DIR__ . "/Database.php";
-require_once __DIR__ . '/OpeningHoursModel.php';
 
 function addBooking(PDO $pdo, $userId, $sessionId, $participants) {
     try {
         $sql = "INSERT INTO `booking`(`bkg_user_id`, `bkg_session_id`, `bkg_nb_of_participants`)
-                        VALUES(:user_id, :session_id, :participants)";
+                VALUES(:user_id, :session_id, :participants)";
 
         $query = $pdo->prepare($sql);
 
@@ -14,10 +12,10 @@ function addBooking(PDO $pdo, $userId, $sessionId, $participants) {
             ':session_id'   => $sessionId,
             ':participants' => $participants
         ]);
-            return $result ? "Votre réservation a été effectuée avec succès." : "Une erreur technique est survenue.";
+
+        return $result ? null : "Une erreur technique est survenue.";
 
     } catch (PDOException $e) {
-
         return "Une erreur technique est survenue.";
-}
+    }
 }
